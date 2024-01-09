a9ffd13e7b90382ecb86aa3a9dfdce7d38d8b496
nikanorsky <kravtsovwindows@gmail.com>
2024-01-09 05:53:41 +0300

    feat: add Add Book page

---
+++ frontend/src/Admin/AddBookPage.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { TextField, Button } from '@mui/material';
+
+const AddBookPage = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [coverUrl, setCoverUrl] = useState('');
+  const [genre, setGenre] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleAddBook = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:5000/books',
+        { title, author, coverUrl, genre, description },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('authToken')}`,
+          },
+        }
+      );
+
+      if (response && response.data) {
+        console.log('Книга успешно добавлена:', response.data);
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error('Ошибка при добавлении книги:', error);
+    }
+  };
+
+  return (
+    <div style={{ padding: '25px' }}>
+      <TextField
+        label="Название книги"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Жанр"
+        value={genre}
+        onChange={(e) => setGenre(e.target.value)}
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Автор"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Описание"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        label="Ссылка на обложку"
+        value={coverUrl}
+        onChange={(e) => setCoverUrl(e.target.value)}
+        fullWidth
+        margin="normal"
+      />
+      <Button onClick={handleAddBook} variant="contained" color="primary">
+        Добавить
+      </Button>
+    </div>
+  );
+};
+
+export default AddBookPage;
