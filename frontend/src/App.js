4a74fb5bff18fdce6bac3fb47dc84816ca3bd654
nikanorsky <kravtsovwindows@gmail.com>
2024-01-09 05:55:01 +0300

    feat: add imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,22 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Footer from './components/Markup/Footer';
+import Header from './components/Markup/Header';
+import LoginPage from './components/Auth/LoginPage';
+import RegisterPage from './components/Auth/RegisterPage';
+import HomePage from './components/Basic/HomePage';
+import FaqPage from './components/Basic/FaqPage';
+import AboutUsPage from './components/Basic/AboutUsPage';
+import BooksPage from './components/Basic/BooksPage';
+import BookDetailPage from './components/Basic/BookDetailPage';
+import AddBookPage from './components/Admin/AddBookPage';
+import EditBookPage from './components/Admin/EditBookPage';
+import AdminBooksPage from './components/Admin/AdminBooksPage';
+import AdminRegisterPage from './components/Auth/AdminRegisterPage';
+import AdminAdminsPage from './components/Admin/AdminAdminsPage';
+import AdminUsersPage from './components/Admin/AdminUsersPage';
+import NotFoundPage from './components/Basic/NotFoundPage';
 
 function App() {
   return (
